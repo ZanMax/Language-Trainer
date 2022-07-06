14e80e680e392c452699ad6583c15cd130e20cb7
maxim <zanoga.maxim@gmail.com>
2022-07-06 10:46:38 +0100

    update menu add services

--- irregular_verbs/urls.py
+++ irregular_verbs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.WordsView.as_view(), name="irregular_verbs"),
+    path("", views.IrregularVerbsView.as_view(), name="irregular_verbs"),
 ]
